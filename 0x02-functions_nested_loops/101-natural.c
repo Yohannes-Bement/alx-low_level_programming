9f2cf179a600137d893fe1a0425a5e5f49a82730
johnjesusgithuberit <johnjesusyohanesbement@gmail.com>
2023-05-24 18:07:09 -0700

    natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,3 +1,8 @@
+/*
+ * File: 101-natural.c
+ * Auth: vincent kip
+ */
+
 #include <stdio.h>
 
 /**
@@ -7,4 +12,17 @@
  * Return: Always 0.
  */
 int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
 
