8c75a6bd0a65d22b0024218743d54bef240a8536
root <johnjesusgithuberit>
2023-08-16 14:18:38 -0700

    array _iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	for (johnjesus = 0; johnjesus < size; johnjesus++)
 	{
-		action(array[i]);
+		action(array[johnjesus]);
 	}
 }
 
