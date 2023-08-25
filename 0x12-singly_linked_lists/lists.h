dd3ac8cb3559c0868f73107acd66c23c8b68baac
root <johnjesusgithuberit>
2023-08-25 01:03:57 -0700

    jhno

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,42 +1,27 @@
-#define LISTS_H
 #ifndef LISTS_H
-
-
-
-
+#define LISTS_H
 
 /**
- * Structure List_s - singly_linked_lists
- * @Structure: string - (malloc'ed string)
- * 
- * @Length: len of the string
- * 
- * @Next: point to the next node
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * 
- * descr: singly_linked_lists  node struct
- * 
- * For holberton PRJ
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-
-    typedef stuct list-s
+typedef struct list_s
 {
-	char*str;
-	
-
+	char *str;
 	unsigned int len;
 	struct list_s *next;
+} list_t;
 
-} list_k;
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-size_t list_len(const list_t *h);
-void free_list(list_t *head);
-size_t list_len(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
-
-
-
+void free_list(list_t *head);
 
 #endif
-
 
